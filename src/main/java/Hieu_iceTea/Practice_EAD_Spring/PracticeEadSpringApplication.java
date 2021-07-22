18801f4a3c39cc62bb023e909598c12eb8dd0347
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-07-22 08:23:04 +0700

    INITIAL: Project

---
+++ src/main/java/Hieu_iceTea/Practice_EAD_Spring/PracticeEadSpringApplication.java
@@ -0,0 +1,13 @@
+package Hieu_iceTea.Practice_EAD_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeEadSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeEadSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hieu_iceTea/Practice_EAD_Spring/PracticeEadSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Hieu_iceTea.Practice_EAD_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeEadSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
